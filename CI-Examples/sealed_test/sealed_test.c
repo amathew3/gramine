4270a78444714ba0c4ca414b8db5889d3327dba5
abin mathew <abinx.mathew@intel.com>
2023-11-15 02:33:08 -0800

    fixup! sealed_file read file test
    Signed-off-by: abin mathew <abinx.mathew@intel.com>

--- CI-Examples/sealed_test/sealed_test.c
+++ CI-Examples/sealed_test/sealed_test.c
@@ -8,11 +8,12 @@
 #include <unistd.h>
 
 int main (int argc, char** argv) {
-   FILE *fp_write,*fp_read;
-   char buffer[100];
+   FILE *fp_write, *fp_read;
+   char c;
    int ret;
    if (argc != 2)
         errx(EXIT_FAILURE, "Usage: %s <plain file>", argv[0]);
+
    size_t arglen=strlen(argv[1]);
    char *input_file=argv[1];
    char* en_filename=malloc(arglen+17); //Enough to hold the whole string
@@ -22,6 +23,7 @@ int main (int argc, char** argv) {
    ret = access(input_file, F_OK);
    if (ret < 0)
       errx(EXIT_FAILURE, "Input file is not accessible");
+
    fp_read = fopen(input_file, "r");
    /* Open file for both reading and writing */
    fp_write = fopen(en_filename, "w+");
@@ -29,14 +31,15 @@ int main (int argc, char** argv) {
 
    /* Seek to the beginning of the file */
    fseek(fp_read, 0, SEEK_SET);
-
-   /* Read and display data */
-   while((fread(buffer, 50, 1, fp_read)>0)){
-        fwrite(buffer , 50 , 1 , fp_write );
+   
+   while((c=fgetc(fp_read))!=EOF) {
+        fputc(c,fp_write);
    }
+
    fseek(fp_write, 0, SEEK_SET);
-   while((fread(buffer, 50, 1, fp_write)>0))
-        printf("%s\n", buffer);
+   while((c=fgetc(fp_write))!=EOF) {
+        printf("%c",c);
+   }
 
    fclose(fp_write);
    fclose(fp_read);
